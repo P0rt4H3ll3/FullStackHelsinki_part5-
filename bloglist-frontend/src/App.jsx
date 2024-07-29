3169183389182550a92d1232353d06d5bc384857
Phillip Radon <phillip.radon@yahoo.com>
2024-07-29 18:59:51 +0200

    5.10: Blog List Frontend, step 10

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -118,9 +118,11 @@ const App = () => {
           <p>{user.name} logged-in</p>
           <button onClick={handleLogout}>Logout</button>
           {blogForm()}
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} transferIdToParent={updateBlog} />
-          ))}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog key={blog.id} blog={blog} transferIdToParent={updateBlog} />
+            ))}
         </div>
       )}
     </div>
