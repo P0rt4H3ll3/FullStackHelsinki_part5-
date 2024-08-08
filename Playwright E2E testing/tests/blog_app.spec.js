fadbce6da9968f53bc87bdc05ed427fa27be2be6
Phillip Radon <phillip.radon@yahoo.com>
2024-08-08 15:56:14 +0200

    5.18: Blog List End To End Testing, step 2

--- Playwright E2E testing/tests/blog_app.spec.js
+++ Playwright E2E testing/tests/blog_app.spec.js
@@ -1,7 +1,15 @@
 const { test, expect, describe, beforeEach } = require('@playwright/test')
 
 describe('Blog App Test', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Tester Mctesti',
+        username: 'TestMaster',
+        password: 'PlayWright1'
+      }
+    })
     await page.goto('/')
   })
 
@@ -9,5 +17,27 @@ describe('Blog App Test', () => {
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
     await expect(page.getByText('Log into BlogApp:')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      const userTextbox = await page.getByTestId('username')
+      const passTextbox = await page.getByTestId('password')
+      await userTextbox.fill('TestMaster')
+      await passTextbox.fill('PlayWright1')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Tester Mctesti logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      const userTextbox = await page.getByTestId('username')
+      const passTextbox = await page.getByTestId('password')
+      await userTextbox.fill('TestMaster')
+      await passTextbox.fill('DoNotTestAtAll')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Tester Mctesti logged in')).not.toBeVisible()
+    })
   })
 })
